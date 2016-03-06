c23668fc8e4c3af5e2cb25a0ae7444a75a42e256
PalermoD <palermo.deschamps@gmail.com>
2016-03-06 16:47:44 -0500

    static assets

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
+import path from 'path';
 import express from 'express';
-import handlebars from 'express=handlebars';
+import handlebars from 'express-handlebars';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import App from './generated/app';
 
 const app = express();
 
+//view templates 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//static assets 
+app.use(express.static(path.resolve(__dirname, '../dist')));
 
 
+//routes
 app.get('/', (request, response) => {
 	response.render('app', {
 		app: ReactDOMServer.renderToString(<App />)
